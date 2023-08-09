bbac8bb0821c3c6b6dd1926974087fa86005b1b3
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-08-09 17:25:35 +0300

    Finish Command Design Pattern

---
+++ WebApp.Command/Commands/ExcelFile.cs
@@ -0,0 +1,62 @@
+﻿using ClosedXML.Excel;
+using System.Data;
+
+namespace WebApp.Command.Commands
+{
+    public class ExcelFile<T>
+    {
+        public readonly List<T> _list;
+
+        public string FileName => $"{typeof(T).Name}.xlsx";
+
+        public string FileType => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+        public ExcelFile(List<T> list)
+        {
+            _list = list;
+        }
+
+        public MemoryStream Create()
+        {
+            var wb = new XLWorkbook();
+
+            var ds = new DataSet();
+
+            ds.Tables.Add(GetTable());
+
+            wb.Worksheets.Add(ds);
+
+            var stream = new MemoryStream();
+
+            wb.SaveAs(stream);
+
+            return stream;
+        }
+
+        private DataTable GetTable()
+        {
+            var table = new DataTable();
+
+            var type = typeof(T);
+
+            type.GetProperties().ToList().ForEach(x =>
+            {
+                table.Columns.Add(x.Name, x.PropertyType);
+            });
+
+            _list.ForEach(x =>
+            {
+                var row = table.NewRow();
+
+                type.GetProperties().ToList().ForEach(y =>
+                {
+                    row[y.Name] = y.GetValue(x, null);
+                });
+
+                table.Rows.Add(row);
+            });
+
+            return table;
+        }
+    }
+}
---
+++ WebApp.Command/Program.cs
@@ -0,0 +1,76 @@
+using BaseProject.Models;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Command.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+
+builder.Services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 4;
+    options.Password.RequireDigit = false;
+    options.User.RequireUniqueEmail = true;
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+})
+    .AddEntityFrameworkStores<AppIdentityDbContext>();
+
+
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+using var scope = app.Services.CreateScope(); //.ServiceProvider.GetRequiredService<AppIdentityDbContext>().Database.Migrate();
+
+var identityDbContext = scope.ServiceProvider.GetRequiredService<AppIdentityDbContext>();
+
+var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+
+identityDbContext.Database.Migrate();
+
+if (!userManager.Users.Any())
+{
+    userManager.CreateAsync(new AppUser() { UserName = "user1", Email = "user1@gmail.com" }, "Password12*").Wait();
+    userManager.CreateAsync(new AppUser() { UserName = "user2", Email = "user2@gmail.com" }, "Password12*").Wait();
+    userManager.CreateAsync(new AppUser() { UserName = "user3", Email = "user3@gmail.com" }, "Password12*").Wait();
+    userManager.CreateAsync(new AppUser() { UserName = "user4", Email = "user4@gmail.com" }, "Password12*").Wait();
+    userManager.CreateAsync(new AppUser() { UserName = "user5", Email = "user5@gmail.com" }, "Password12*").Wait();
+
+    Enumerable.Range(1, 30).ToList().ForEach(x =>
+    {
+        identityDbContext.Products.Add(new Product() { Name = $"kalem {x}", Price = x * 100, Stock = x + 50 });
+    });
+    identityDbContext.SaveChanges();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
